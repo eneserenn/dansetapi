a90d437b64a462365f0a0dabb30a63afb985cc83
Onur AKÇAY <onurakcay.tr@gmail.com>
2020-11-09 21:38:10 +0300

    test

--- dansetapi/app/Http/Controllers/QuestionController.php
+++ dansetapi/app/Http/Controllers/QuestionController.php
@@ -9,31 +9,30 @@ use App\Models\Quiz;
 
 class QuestionController extends Controller
 {
-    public function create(Request $req){
+    public function create(Request $req)
+    {
 
-        $question = new QuizQuestion; 
-        $question -> quiz_id = $req -> quiz_id;
-        $question -> question = $req -> question;
-        $question -> right_count = $req -> right_count;
-        $question -> wrong_count = $req -> wrong_count;
-  
-        $question -> save();
+        $question = new QuizQuestion;
+        $question->quiz_id = $req->quiz_id;
+        $question->question = $req->question;
+        $question->right_count = $req->right_count;
+        $question->wrong_count = $req->wrong_count;
 
+        $question->save();
     }
 
-    public function allquestion($id){
-
-       $questions = Quiz::find($id) -> getQuestion() -> get();
-       
-       foreach($questions as $question){
-
-       $answers = $question -> getAnswer()-> get();
-
-       return response()->json([
-        'question' => $question -> question,
-        'answers' => $answers
-    ]);
-       }
-
+    public function allquestion($id)
+    {
+        $questions = Quiz::find($id)->getQuestion()->get();
+        $array = [];
+        foreach ($questions as $question) {
+            $answers = $question->getAnswer()->get();
+            $soru = array(
+                'question' => $question->question,
+                'answers' => $answers
+            );
+            array_push($array, $soru);
+        }
+        return response()->json(['questions' => $array]);
     }
 }
